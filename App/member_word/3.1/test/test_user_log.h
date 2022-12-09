29cfaa1f637bfc90d3c063c498911af31490feb1
“changjingjing” <2249077862@qq.com>
2022-12-09 18:07:43 +0800

    测试用户登录的正确性，获取用户每日数据

---
+++ App/member_word/3.1/test/test_user_log.h
@@ -0,0 +1,14 @@
+//
+// Created by 22490 on 2022/12/9.
+//
+
+#ifndef INC_3_1__TEST_USER_LOG_H_
+#define INC_3_1__TEST_USER_LOG_H_
+#include <iostream>
+#include <bits/stdc++.h>
+#include <mysql.h>
+
+extern void test_user_log();
+
+
+#endif //INC_3_1__TEST_USER_LOG_H_
