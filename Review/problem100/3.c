3b4c7493bff7d696ff4260049091038825ee462a
“changjingjing” <2249077862@qq.com>
2023-03-17 08:34:00 +0800

    注释第三题，完成第四题

--- Review/problem100/3.c
+++ Review/problem100/3.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <math.h>
-// 题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
-
-int main()
-{
-	int i = 0;
-	for (i;; i++)
-	{
-		if ((double)(int)sqrt(i + 100) == sqrt(i + 100)
-			&& (double)(int)sqrt(i + 268) == sqrt(i + 268))
-		{
-			printf("num is:%d\n", i);
-			break;
-		}
-	}
-	return 0;
-}
+//#include <stdio.h>
+//#include <math.h>
+//// 题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i;; i++)
+//	{
+//		if ((double)(int)sqrt(i + 100) == sqrt(i + 100)
+//			&& (double)(int)sqrt(i + 268) == sqrt(i + 268))
+//		{
+//			printf("num is:%d\n", i);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
--- Review/problem100/4.c
+++ Review/problem100/4.c
@@ -2,9 +2,20 @@
 
 //题目：输入某年某月某日，判断这一天是这一年的第几天？
 
+int isPrime(int year)
+{
+	return year%400 == 0 || (year%100 != 0 && year%4 == 0);
+}
+
 int main()
 {
-	int month[13] = {0, 31, 28,31,30,31,30,31,31,30,31,30,31};
-
+	int months[13] = { 0, 0, 31, 59, 89, 120, 150, 181, 212, 242, 273, 303, 334};
+	int year = 0, month = 0, day = 0, result = 0;
+	scanf("%d%d%d", &year, &month, &day);
+	result = months[month];
+	if (month>2 && isPrime(year)){
+		result += 1;
+	}
+	printf("这一天是第%d天", result);
 	return 0;
 }
