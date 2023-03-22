7bd1d1f99e57696c5f03bb997c1365759768a9e8
“changjingjing” <2249077862@qq.com>
2023-03-22 10:29:17 +0800

    完成30到35

---
+++ Review/problem100/33.c
@@ -0,0 +1,24 @@
+//// 题目：判断一个数字是否为质数。
+//#include <stdio.h>
+//#include <math.h>
+//
+//int isPrime(int n)
+//{
+//	int i;
+//	for (i = 2; i < sqrt(n); i++)
+//		if (n % i == 0)
+//			return 0;
+//
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	if (isPrime(n))
+//		printf("是素数\n");
+//	else
+//		printf("不是是素数\n");
+//	return 0;
+//}
---
+++ Review/problem100/35.c
@@ -0,0 +1,24 @@
+//// 题目：字符串反转，如将字符串 "www.runoob.com" 反转为 "moc.boonur.www"。
+//#include <stdio.h>
+//#include <string.h>
+//
+//void swap(char* s1, char* s2)
+//{
+//	char ch = *s1;
+//	*s1 = *s2;
+//	*s2 = ch;
+//}
+//
+//int main()
+//{
+//	char str[100];
+//	gets(str);
+//	int i = 0, j = strlen(str) - 1;
+//	while (i < j)
+//	{
+//		swap(&str[i], &str[j]);
+//		i++,j--;
+//	}
+//	printf("%s", str);
+//	return 0;
+//}
