5124f015c8577ff372b4a11f54a4aaf590facd0b
“changjingjing” <2249077862@qq.com>
2023-03-26 14:54:15 +0800

    完成51到55

---
+++ Review/problem100/31-60/51.c
@@ -0,0 +1,12 @@
+//// 题目：学习使用按位与 &。
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b;
+//	a = 077;
+//	b = a & 3;
+//	printf("a & b(decimal) 为 %d \n", b);
+//	b &= 7;
+//	printf("a & b(decimal) 为 %d \n", b);
+//	return 0;
+//}
---
+++ Review/problem100/31-60/54.c
@@ -0,0 +1,15 @@
+//// 题目：取一个整数 a 从右端开始的 4～7 位。
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n,ans = 0xf;
+//	scanf("%d", &n);
+//
+//	n >>= 4;
+//	ans &= n;
+//
+//	printf("%d", ans);
+//
+//	return 0;
+//}
